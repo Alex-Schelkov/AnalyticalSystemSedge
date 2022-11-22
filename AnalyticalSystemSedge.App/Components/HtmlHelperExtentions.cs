7e53d145ed802d83746d8bc286ba99429c389317
Щелков Алексей Иванович <orenburgstyle@mail.ru>
2022-11-23 00:47:09 +0500

    Вынос теста их htmlhelper

--- AnalyticalSystemSedge.App/Components/HtmlHelperExtentions.cs
+++ AnalyticalSystemSedge.App/Components/HtmlHelperExtentions.cs
@@ -10,50 +10,11 @@ namespace AnalyticalSystemSedge.App.Components
 {
     public static class HtmlHelperExtentions
     {
-        public static HtmlString Chart()
+        public static HtmlString Chart(Chart chart)
         {
             StringBuilder htmlCollector = new StringBuilder();
 
-            //--- УСТАНОВКА параметов char
-            Chart chart = new Chart();
-            
-            chart.Sigment = new Sigment() { Id = 1};
-            chart.Id = 1;
-            chart.Type = TypeChart.bar;
-
-            //опции
-            OptionsChart options = new OptionsChart();
-            options.Vertical = true;
-            options.Cumulative = true;
-            options.Text = "Количество обращений (в штуках) [на 12.11.2022]";
-            chart.Options = options;
-         
-
-            List<DataChart> datas = new List<DataChart>();
-
-        
-            List <DataSetChart> datasets = new List<DataSetChart>();
-            datasets.Add(new DataSetChart() { Caption = "Благодарности", Values = new List<double>() { 3, 6, 7, 7, 11, 15, 3, 15, 13, 10,  8, 5 }, Color = Color.Orange });
-           datasets.Add(new DataSetChart() { Caption = "Жалобы",         Values = new List<double>() { 1, 2, 3, 1,  5,  7, 9, 11, 1, 15, 12, 9 }, Color = Color.HotPink });
-            datasets.Add(new DataSetChart() { Caption = "Нейтральные отзывы", Values = new List<double>() { 6, 3, 5, 7, 9, 12, 14, 4, 3, 2, 1, 2 }, Color = Color.Lime });
-
-
-            List<Labels> labels = new List<Labels>();
-            labels.Add(new Labels() { Text = "КЦСОН Северный округ Оренбург" });
-            labels.Add(new Labels() { Text = "КЦСОН Южный округ Оренбург"});
-            labels.Add(new Labels() { Text = "КЦСОН Орск" });
-            labels.Add(new Labels() { Text = "КЦСОН Сорочинск" });
-            labels.Add(new Labels() { Text = "КЦСОН Соль-Илецк" });
-            labels.Add(new Labels() { Text = "КЦСОН Бузулук" });
-            labels.Add(new Labels() { Text = "КЦСОН Бугуруслан" });
-            labels.Add(new Labels() { Text = "КЦСОН Гай" });
-            labels.Add(new Labels() { Text = "КЦСОН Ясный" });
-            labels.Add(new Labels() { Text = "КЦСОН Медногорск" });
-            labels.Add(new Labels() { Text = "КЦСОН Кувандык" });
-            labels.Add(new Labels() { Text = "КЦСОН Оренбургский" });
-
-
-            datas.Add(new DataChart() { labels = labels , dataSetCharts= datasets});
+            var datas = chart.Datas;
             //---------------------------------
 
             //--размеры доделать
--- AnalyticalSystemSedge.App/Pages/Index.cshtml.cs
+++ AnalyticalSystemSedge.App/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AnalyticalSystemSedge.App.Data.Charts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Drawing;
 
 namespace AnalyticalSystemSedge.App.Pages
 {
@@ -9,6 +11,8 @@ namespace AnalyticalSystemSedge.App.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public Chart TestChart { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -16,6 +20,53 @@ namespace AnalyticalSystemSedge.App.Pages
 
         public void OnGet()
         {
+
+            Chart chart = new Chart();
+        //--- УСТАНОВКА параметов char
+
+
+        chart.Sigment = new Sigment() { Id = 1 };
+            chart.Id = 1;
+            chart.Type = TypeChart.bar;
+
+            //опции
+            OptionsChart options = new OptionsChart();
+            options.Vertical = true;
+            options.Cumulative = true;
+            options.Text = "Количество обращений (в штуках) [на 12.11.2022]";
+            chart.Options = options;
+
+
+            List<DataChart> datas = new List<DataChart>();
+
+
+            List<DataSetChart> datasets = new List<DataSetChart>();
+            datasets.Add(new DataSetChart() { Caption = "Благодарности", Values = new List<double>() { 3, 6, 7, 7, 11, 15, 3, 15, 13, 10, 8, 5 }, Color = Color.Orange });
+            datasets.Add(new DataSetChart() { Caption = "Жалобы", Values = new List<double>() { 1, 2, 3, 1, 5, 7, 9, 11, 1, 15, 12, 9 }, Color = Color.HotPink });
+            datasets.Add(new DataSetChart() { Caption = "Нейтральные отзывы", Values = new List<double>() { 6, 3, 5, 7, 9, 12, 14, 4, 3, 2, 1, 2 }, Color = Color.Lime });
+
+
+            List<Labels> labels = new List<Labels>();
+            labels.Add(new Labels() { Text = "КЦСОН Северный округ Оренбург" });
+            labels.Add(new Labels() { Text = "КЦСОН Южный округ Оренбург" });
+            labels.Add(new Labels() { Text = "КЦСОН Орск" });
+            labels.Add(new Labels() { Text = "КЦСОН Сорочинск" });
+            labels.Add(new Labels() { Text = "КЦСОН Соль-Илецк" });
+            labels.Add(new Labels() { Text = "КЦСОН Бузулук" });
+            labels.Add(new Labels() { Text = "КЦСОН Бугуруслан" });
+            labels.Add(new Labels() { Text = "КЦСОН Гай" });
+            labels.Add(new Labels() { Text = "КЦСОН Ясный" });
+            labels.Add(new Labels() { Text = "КЦСОН Медногорск" });
+            labels.Add(new Labels() { Text = "КЦСОН Кувандык" });
+            labels.Add(new Labels() { Text = "КЦСОН Оренбургский" });
+
+
+            datas.Add(new DataChart() { labels = labels, dataSetCharts = datasets });
+
+            chart.Datas = datas;
+
+            TestChart = chart;
+
 
         }
     }
